e90e7bdbcb39669c4e8822c3f5ea856ef93d44d0
Fernando Henrique Bandeira <fernando.bandeira94@gmail.com>
2017-10-03 11:13:26 -0300

    Migrations

---
+++ database/migrations/2017_10_03_140852_create_coffees_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoffeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coffees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamp('schedule');
+            $table->integer('profile_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('profile_id')->references('id')->on('profiles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coffees');
+    }
+}
